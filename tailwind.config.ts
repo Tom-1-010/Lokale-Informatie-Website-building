3e430571f6909734f6952f9c241708224d14024e
Tom-1-010 <tommaassluis@outlook.com>
2026-07-07 19:20:32 +0200

    Update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -21,7 +21,7 @@ const config: Config = {
         accent_soft: "#FFC5DE",
       },
       fontFamily: {
-        sans: ["var(--font-inter)", "system-ui", "sans-serif"],
+        sans: ["var(--font-rubik)", "system-ui", "sans-serif"],
         serif: ["var(--font-merriweather)", "Georgia", "serif"],
       },
     },
